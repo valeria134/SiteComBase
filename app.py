838f33e7202a8939a5ee17a41db8281f528acbd7
valeria134 <valeria134@gmail.com>
2019-12-04 20:41:26 -0300

    commit

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from formulario import CadastroForm, LoginForm
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user
  
 app = Flask (__name__)
 app.config['SECRET_KEY'] = 'dkshvfdikhgolfhvljh'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MeuSite.db'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(80), unique=True, nullable=True)
+    email = db.Column(db.String(12), unique=True, nullable=False)
+    senha = db.Column(db.String(120), nullable=False)
+  
+@property
+    
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -11,6 +25,14 @@ def home():
 @app.route('/cadastro', methods=["GET", "POST"])
 def Cadastro():
     formulario=CadastroForm()
+    if request.method == 'POST':
+        user = User()
+        user.usuario = request.form['usuario']
+        user.email = request.form['email']
+        user.senha = request.form['senha']
+        db.session.add(user)
+        db.session.commit()
+
     return render_template(
         'cadastro.html',
         formulario=formulario
--- formulario.py
+++ formulario.py
@@ -1,18 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email,EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class CadastroForm(FlaskForm):
-    name= StringField("Nome Completo", validators= [DataRequired()])
+    usuario= StringField("Usuario", validators= [DataRequired()])
     email= StringField("Email", validators= [DataRequired(),Email()])
     senha= PasswordField("Senha",[DataRequired(),EqualTo('confirm',message='Senhas diferentes')])
     confirm= PasswordField('Repita a senha')
-    botao=SubmitField('Enviar')
- 
+    submit = SubmitField('Cadastrar')
+    
 
 
 class LoginForm(FlaskForm):
     email= StringField("Email", validators= [DataRequired(),Email()])
     senha= PasswordField("Senha",[DataRequired(),EqualTo('confirm',message='Senhas diferentes')])
-    botao=SubmitField('Enviar')
+    remember_me= BooleanField('Lembrar_senha')
+    submit = SubmitField('Login')
 
